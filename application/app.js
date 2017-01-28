9c9a15b5e2c2b762b3b4a35dfe77b8fd05c1194e
mudit jain <1996mudit1502@gmail.com>
2017-01-28 16:52:58 +0530

    issues fixes

--- application/app.js
+++ application/app.js
@@ -6,6 +6,11 @@ var myApp = angular.module('mwhApp', ['ngRoute'], function($interpolateProvider)
 
 myApp.run(function ($rootScope, $location) {
   $rootScope.$on("$routeChangeStart", function (event) {
+    // scrolling to top on changing routes starts
+      window.scrollTo(0, 0);
+    // scrolling to top on changing routes ends
+
+
     var currLoc = $location.path();
     // condition only for home page
     if(currLoc == "/"){

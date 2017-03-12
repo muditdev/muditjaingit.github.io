1596e1b4e6aa204ebda22cd3cd3a271a64d9a1e0
Mudit Jain <1996mudit1502@gmail.com>
2017-03-12 11:32:20 +0530

    gemfile

--- application/config.js
+++ application/config.js
@@ -1,27 +1,27 @@
-myApp.config(['$routeProvider','$locationProvider','$httpProvider',function($routeProvider,$locationProvider,$httpProvider) {
-    //$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-    $routeProvider
-      .when('/', {
-            templateUrl: 'views/home.html',
-            controller: 'HomeCtrl as HomeCtrl'
-      })
-      .when('/services', {
-            templateUrl: 'views/services.html',
-            controller: 'ServCtrl as ServCtrl'
-      })
-      .when('/projects', {
-            templateUrl: 'views/projects.html',
-            controller: 'ProjectsCtrl as ProjectsCtrl'
-      })
-      .when('/products', {
-            templateUrl: 'views/products.html',
-            controller: 'ProductsCtrl as ProductsCtrl'
-      })
-      .when('/blog', {
-            templateUrl: 'views/blog.html',
-            controller: 'BlogCtrl as BlogCtrl'
-      })
-      .otherwise({
-            redirectTo: '/'
-      });
+myApp.config(['$routeProvider', '$locationProvider', '$httpProvider', function($routeProvider, $locationProvider, $httpProvider) {
+      //$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+      $routeProvider
+            .when('/', {
+                  templateUrl: 'views/home.html',
+                  controller: 'HomeCtrl as HomeCtrl'
+            })
+            .when('/services', {
+                  templateUrl: 'views/services.html',
+                  controller: 'ServCtrl as ServCtrl'
+            })
+            .when('/projects', {
+                  templateUrl: 'views/projects.html',
+                  controller: 'ProjectsCtrl as ProjectsCtrl'
+            })
+            .when('/products', {
+                  templateUrl: 'views/products.html',
+                  controller: 'ProductsCtrl as ProductsCtrl'
+            })
+            .when('/blog', {
+                  templateUrl: 'views/blog.html',
+                  controller: 'BlogCtrl as BlogCtrl'
+            })
+            .otherwise({
+                  redirectTo: '/'
+            });
 }]);
--- application/main.js
+++ application/main.js
@@ -1,20 +1,19 @@
-$(document).ready(function(){
-    nlInput();
-    blocksBg();
-    // setting background over scroll
-    $(window).scroll(blocksBg);
-    mobileNav();
+$(document).ready(function() {
+  nlInput();
+  blocksBg();
+  // setting background over scroll
+  $(window).scroll(blocksBg);
+  mobileNav();
 });
 
 
 
-
-function nlInput(){
-  $('.nl-input input').on('focus', function(){
+function nlInput() {
+  $('.nl-input input').on('focus', function() {
     $(this).parent('.nl-input').addClass('active');
   });
-  $('.nl-input input').on('blur', function(){
-    if($(this).val() == ''){
+  $('.nl-input input').on('blur', function() {
+    if ($(this).val() == '') {
       $(this).parent('.nl-input').removeClass('active');
     }
   });
@@ -24,43 +23,42 @@ function nlInput(){
 //project product and service block background -------------------
 function blocksBg() {
   // prevent to run if element not exist
-  if($('[data-bgcolor]').length){
+  if ($('[data-bgcolor]').length) {
     var topScroll = $(window).scrollTop();
     var topOffset = $('[data-bgcolor]').parent().offset().top;
-    var offsetTolr = $(window).innerHeight()/2;
-      // running each function on project product and service block
-      console.log(offsetTolr);
-      $('[data-bgcolor]').each(function(i){
-        var $block = $(this);
-        var bgColor = $(this).data('bgcolor');
+    var offsetTolr = $(window).innerHeight() / 2;
+    // running each function on project product and service block
+    console.log(offsetTolr);
+    $('[data-bgcolor]').each(function(i) {
+      var $block = $(this);
+      var bgColor = $(this).data('bgcolor');
 
-        if(topScroll > topOffset - offsetTolr ){
-          setTimeout(function(){
-            $block.css({
-              'background-color': bgColor,
-              'opacity' : 1
-            });
-          }, i*400);
-        }//if statement ends
-      });//each event ends
+      if (topScroll > topOffset - offsetTolr) {
+        setTimeout(function() {
+          $block.css({
+            'background-color': bgColor,
+            'opacity': 1
+          });
+        }, i * 400);
+      } //if statement ends
+    }); //each event ends
   }
-}//blocksBg function ends
+} //blocksBg function ends
 
 
-function mobileNav(){
+function mobileNav() {
   $(".trigger").click(function() {
-    if(!$('body').hasClass("mmOpen")){
+    if (!$('body').hasClass("mmOpen")) {
       $(".trigger").addClass("cross");
       $('body').addClass("mmOpen");
       navEleEffect();
-      setTimeout(function(){
+      setTimeout(function() {
         $('.mn-header .logo').addClass('show');
         $('.mn-header .logo').addClass('animated zoomIn');
       }, 1000);
-    }
-    else {
+    } else {
       clearAni();
-      setTimeout(function(){
+      setTimeout(function() {
         $('.mn-header .logo').removeClass('show');
         $('.mn-header .logo').removeClass('animated zoomIn');
       }, 1000);
@@ -69,25 +67,25 @@ function mobileNav(){
     }
   });
 
-  function navEleEffect(){
+  function navEleEffect() {
     var b = 0;
-    $('.mn-nav').find('li a').each(function(i){
-      b = (i == 5) ? 0 : 255 - 33*i;
+    $('.mn-nav').find('li a').each(function(i) {
+      b = (i == 5) ? 0 : 255 - 33 * i;
       var getBg = $('.mn-header').css('background-color');
       var bgArr = getBg.split("rgb")[1].replace("(", "").replace(")", "").split(',');
-      var bgC = 'rgb('+bgArr[0]+','+bgArr[1]+','+b+')';
+      var bgC = 'rgb(' + bgArr[0] + ',' + bgArr[1] + ',' + b + ')';
       // console.log(bgC);
       setTimeout(function() {
         // console.log($(this));
         $('.mn-nav').find('li').eq(i).find('a').addClass('show');
         $('.mn-nav').find('li').eq(i).find('a').addClass('animated bounceInDown')
         $('.mn-nav').find('li').eq(i).find('a').css('background-color', bgC);
-      }, i*200)
+      }, i * 200)
     });
   }
 
-  function clearAni(){
-    setTimeout(function(){
+  function clearAni() {
+    setTimeout(function() {
       $('.mn-nav li a').removeClass('show');
       $('.mn-nav').find('li a').removeClass('animated bounceInDown')
       $('.mn-nav').find('li a').css('background-color', '');
@@ -97,9 +95,9 @@ function mobileNav(){
 
 
   //closing with link click
-  $('.mn-nav li a').click(function(){
+  $('.mn-nav li a').click(function() {
     clearAni();
-    setTimeout(function(){
+    setTimeout(function() {
       $('.mn-header .logo').removeClass('show');
       $('.mn-header .logo').removeClass('animated zoomIn');
     }, 1000);
